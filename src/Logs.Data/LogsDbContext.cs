b6b7343adb5bbf0f9c28a869b449bc0cb7b6a301
Ivan <madritistaaa@gmail.com>
2017-02-28 21:44:30 +0200

    Add migrations

---
+++ src/Logs.Data/LogsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using Logs.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Logs.Data
+{
+    public class LogsDbContext : IdentityDbContext<User>
+    {
+        public LogsDbContext()
+            : base("LogsDb", throwIfV1Schema: false)
+        {
+            this.Database.CreateIfNotExists();
+        }
+
+        public static LogsDbContext Create()
+        {
+            return new LogsDbContext();
+        }
+    }
+}
