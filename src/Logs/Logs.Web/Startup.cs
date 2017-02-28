4a7279213149112a11f05e59bd56f2e74224de3a
Ivan <madritistaaa@gmail.com>
2017-02-28 18:22:58 +0200

    Add web project

---
+++ src/Logs/Logs.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Logs.Web.Startup))]
+namespace Logs.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
