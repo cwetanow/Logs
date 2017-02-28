6622dfaa910104f18cb68d38216978d12bcf09ff
Ivan <madritistaaa@gmail.com>
2017-02-28 21:43:52 +0200

    Finish auth provider

--- src/Logs.Authentication/Contracts/IAuthenticationProvider.cs
+++ src/Logs.Authentication/Contracts/IAuthenticationProvider.cs
@@ -1,7 +1,19 @@
-﻿namespace Logs.Authentication.Contracts
+﻿using Logs.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace Logs.Authentication.Contracts
 {
     public interface IAuthenticationProvider
     {
         bool IsAuthenticated { get; }
+
+        IdentityResult CreateUser(User user, string password);
+
+        void SignIn(User user, bool isPersistent, bool rememberBrowser);
+
+        SignInStatus SignInWithPassword(string email, string password, bool rememberMe, bool shouldLockout);
+
+        void SignOut();
     }
 }
--- src/Logs.Authentication/HttpContextAuthenticationProvider.cs
+++ src/Logs.Authentication/HttpContextAuthenticationProvider.cs
@@ -1,5 +1,9 @@
 ﻿using System.Web;
 using Logs.Authentication.Contracts;
+using Logs.Authentication.Managers;
+using Logs.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace Logs.Authentication
 {
@@ -11,6 +15,44 @@ namespace Logs.Authentication
             {
                 return HttpContext.Current.User.Identity.IsAuthenticated;
             }
+        }
+
+        public IdentityResult CreateUser(string email, string password)
+        {
+            var user = new User { Email = email, UserName = email };
+            var manager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+            var result = manager.Create(user, password);
+
+            return result;
+        }
+
+        public IdentityResult CreateUser(User user, string password)
+        {
+            var manager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+            var result = manager.Create(user, password);
+
+            return result;
+        }
+
+        public void SignIn(User user, bool isPersistent, bool rememberBrowser)
+        {
+            var manager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+
+            manager.SignIn(user, isPersistent, rememberBrowser);
+        }
+
+        public SignInStatus SignInWithPassword(string email, string password, bool rememberMe, bool shouldLockout)
+        {
+            var manager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+
+            return manager.PasswordSignIn(email, password, rememberMe, shouldLockout);
+        }
+
+        public void SignOut()
+        {
+            HttpContext.Current.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
         }
     }
 }
