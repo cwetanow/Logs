ce569f42f21d41b22571bba24fab6b6f5a36a9b9
Ivan <madritistaaa@gmail.com>
2017-03-01 15:51:16 +0200

    Add ctor to logs service

--- src/Logs.Services/LogsService.cs
+++ src/Logs.Services/LogsService.cs
@@ -1,13 +1,23 @@
-﻿using Logs.Models;
+﻿using Logs.Data.Contracts;
+using Logs.Models;
 using Logs.Services.Contracts;
 
 namespace Logs.Services
 {
     public class LogsService : ILogService
     {
+        private readonly IRepository<TrainingLog> logRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public LogsService(IRepository<TrainingLog> logRepository, IUnitOfWork unitOfWork)
+        {
+            this.logRepository = logRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
         public TrainingLog GeTrainingLogById(int id)
         {
-            return null;
+            return this.logRepository.GetById(id);
         }
     }
 }
