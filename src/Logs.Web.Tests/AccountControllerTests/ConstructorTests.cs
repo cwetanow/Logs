e384d25ec9a6ef5f1449732fa75a9a802e19095b
Ivan <madritistaaa@gmail.com>
2017-03-01 15:24:15 +0200

    Account controller constructor tests

---
+++ src/Logs.Web.Tests/AccountControllerTests/ConstructorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Logs.Authentication.Contracts;
+using Logs.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Web.Tests.AccountControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange, Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new AccountController(null));
+        }
+
+        [Test]
+        public void TestConstructor_PassProvider_ShouldNotThrow()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            // Act, Assert
+            Assert.DoesNotThrow(() => new AccountController(mockedProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            // Act
+            var controller = new AccountController(mockedProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+    }
+}
