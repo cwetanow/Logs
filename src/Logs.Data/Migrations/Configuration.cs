d2d8288bda0cb94a0afb2fdc8ec6b53b3f1c7707
Ivan <madritistaaa@gmail.com>
2017-03-01 14:09:53 +0200

    Add admin role in migrations

--- src/Logs.Data/Migrations/Configuration.cs
+++ src/Logs.Data/Migrations/Configuration.cs
@@ -1,12 +1,14 @@
+using System.Linq;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace Logs.Data.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Logs.Data.LogsDbContext>
     {
+        private const string AdminRoleName = "administrator";
+
         public Configuration()
         {
             this.AutomaticMigrationsEnabled = true;
@@ -14,18 +16,10 @@ namespace Logs.Data.Migrations
 
         protected override void Seed(Logs.Data.LogsDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            if (!context.Roles.Any(r => r.Name.Equals(AdminRoleName)))
+            {
+                context.Roles.Add(new IdentityRole(AdminRoleName));
+            }
         }
     }
 }
