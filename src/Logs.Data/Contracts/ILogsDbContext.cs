ca07eb10b8339c8f0a99095094a24a0b1e6e58ef
Ivan <madritistaaa@gmail.com>
2017-03-01 14:35:17 +0200

    Add dbcontext interface

---
+++ src/Logs.Data/Contracts/ILogsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace Logs.Data.Contracts
+{
+    public interface ILogsDbContext
+    {
+        DbSet<TEntity> Set<TEntity>()
+            where TEntity : class;
+
+        int SaveChanges();
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity)
+            where TEntity : class;
+    }
+}
