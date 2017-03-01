960b626b7e82078b76646510f4e6abc617b385c4
Ivan <madritistaaa@gmail.com>
2017-03-01 18:06:39 +0200

    Profile controller

---
+++ src/Logs.Web/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Logs.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public ActionResult Index(string name)
+        {
+            return View();
+        }
+    }
+}
