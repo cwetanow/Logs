40270c01bc7928c47a5a8b8195c02881d6552f7c
Ivan <madritistaaa@gmail.com>
2017-03-02 17:46:40 +0200

    Log service constructor tests

---
+++ src/Logs.Services.Tests/LogsServiceTests/ConstructorTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Logs.Data.Contracts;
+using Logs.Factories;
+using Logs.Models;
+using Logs.Providers.Contracts;
+using Logs.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Services.Tests.LogsServiceTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassLogRepositoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedLogFactory = new Mock<ITrainingLogFactory>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new LogsService(null,
+                mockedUnitOfWork.Object,
+                mockedLogFactory.Object,
+                mockedUserService.Object,
+                mockedDateTimeProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassUnitOfWorkNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
+            var mockedLogFactory = new Mock<ITrainingLogFactory>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new LogsService(mockedLogRepository.Object,
+                null,
+                mockedLogFactory.Object,
+                mockedUserService.Object,
+                mockedDateTimeProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassLogFactoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new LogsService(mockedLogRepository.Object,
+                mockedUnitOfWork.Object,
+                null,
+                mockedUserService.Object,
+                mockedDateTimeProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassUserServiceNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedLogFactory = new Mock<ITrainingLogFactory>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new LogsService(mockedLogRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedLogFactory.Object,
+                null,
+                mockedDateTimeProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassDateTimeProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedLogFactory = new Mock<ITrainingLogFactory>();
+            var mockedUserService = new Mock<IUserService>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new LogsService(mockedLogRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedLogFactory.Object,
+                mockedUserService.Object,
+                null));
+        }
+
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldNotThrow()
+        {
+            // Arrange
+            var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedLogFactory = new Mock<ITrainingLogFactory>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            // Act, Assert
+            Assert.DoesNotThrow(() => new LogsService(mockedLogRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedLogFactory.Object,
+                mockedUserService.Object,
+                mockedDateTimeProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedLogFactory = new Mock<ITrainingLogFactory>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            // Act
+            var service = new LogsService(mockedLogRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedLogFactory.Object,
+                mockedUserService.Object,
+                mockedDateTimeProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(service);
+        }
+    }
+}
