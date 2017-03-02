d342aac32b13fbde7edc87cb4f3a02838a662f0b
Ivan <madritistaaa@gmail.com>
2017-03-02 12:10:49 +0200

    Add jquery bundles

--- src/Logs.Web/App_Start/BundleConfig.cs
+++ src/Logs.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace Logs.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-unobtrusive").Include(
+                "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
