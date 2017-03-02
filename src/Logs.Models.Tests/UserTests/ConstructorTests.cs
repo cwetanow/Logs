e30541a5a39ff4147d9d419adb9cf9a86115ca51
Ivan <madritistaaa@gmail.com>
2017-03-02 22:13:40 +0200

    User model tests

---
+++ src/Logs.Models.Tests/UserTests/ConstructorTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace Logs.Models.Tests.UserTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [TestCase("username", "email", "name")]
+        [TestCase("other-username", "other-email", "other-name")]
+        public void TestConstructor_ShouldSetUsernameCorrectly(string username, string email, string name)
+        {
+            // Arrange. Act
+            var user = new User(username, email, name);
+
+            // Assert
+            Assert.AreEqual(user.UserName, username);
+        }
+
+        [TestCase("username", "email", "name")]
+        [TestCase("other-username", "other-email", "other-name")]
+        public void TestConstructor_ShouldSetEmailCorrectly(string username, string email, string name)
+        {
+            // Arrange. Act
+            var user = new User(username, email, name);
+
+            // Assert
+            Assert.AreEqual(user.Email, email);
+        }
+
+        [TestCase("username", "email", "name")]
+        [TestCase("other-username", "other-email", "other-name")]
+        public void TestConstructor_ShouldSetNameCorrectly(string username, string email, string name)
+        {
+            // Arrange. Act
+            var user = new User(username, email, name);
+
+            // Assert
+            Assert.AreEqual(user.Name, name);
+        }
+    }
+}
---
+++ src/Logs.Models.Tests/UserTests/PropertiesTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Logs.Models.Tests.UserTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [Test]
+        public void TestProperties_ShouldSetLogCorrectly()
+        {
+            // Arrange
+            var log = new TrainingLog();
+            var user = new User();
+
+            // Act
+            user.Log = log;
+
+            // Assert
+            Assert.AreSame(log, user.Log);
+        }
+
+        [Test]
+        public void TestProperties_ShouldSetCommentsCorrectly()
+        {
+            // Arrange
+            var comments = new List<Comment>();
+            var user = new User();
+
+            // Act
+            user.Comments = comments;
+
+            // Assert
+            CollectionAssert.AreEqual(comments, user.Comments);
+        }
+
+        [Test]
+        public void TestProperties_ShouldSetEntriesCorrectly()
+        {
+            // Arrange
+            var entries = new List<LogEntry>();
+            var user = new User();
+
+            // Act
+            user.Entries = entries;
+
+            // Assert
+            CollectionAssert.AreEqual(entries, user.Entries);
+        }
+    }
+}
