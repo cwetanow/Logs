745b9e38a49c2b3272100fcfeb06f522dc4519b3
Ivan <madritistaaa@gmail.com>
2017-03-02 20:46:43 +0200

    Controller function for my log

--- src/Logs.Web/Controllers/ProfileController.cs
+++ src/Logs.Web/Controllers/ProfileController.cs
@@ -1,13 +1,44 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using Logs.Authentication.Contracts;
+using Logs.Services.Contracts;
 
 namespace Logs.Web.Controllers
 {
     public class ProfileController : Controller
     {
-        public ActionResult Index(string name)
+        private readonly IAuthenticationProvider provider;
+        private readonly IUserService userService;
+
+        public ProfileController(IAuthenticationProvider provider, IUserService userService)
         {
-            // TODO
-            return View();
+            if (provider == null)
+            {
+                throw new ArgumentNullException("provider");
+            }
+
+            if (userService == null)
+            {
+                throw new ArgumentNullException("userService");
+            }
+
+            this.userService = userService;
+            this.provider = provider;
+        }
+
+        //public ActionResult Index(string name)
+        //{
+        //    // TODO
+        //    return View("~/Views/Home/Index");
+        //}
+
+        [HttpGet]
+        public RedirectToRouteResult MyLog()
+        {
+            var currentUserId = this.provider.CurrentUserId;
+            var currentUser = this.userService.GetUserById(currentUserId);
+
+            return this.RedirectToAction("Details", "Logs", new { id = currentUser.Log.LogId });
         }
     }
 }
