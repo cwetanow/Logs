07543d5f31c3dfe59f142e1bfbc92cd7d26e7e45
Ivan <madritistaaa@gmail.com>
2017-03-02 14:30:16 +0200

    Repository add tests

---
+++ src/Logs.Data.Tests/EfGenericRepositoryTests/AddTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity.Infrastructure;
+using Logs.Data.Contracts;
+using Logs.Data.Tests.Fakes;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Data.Tests.EfGenericRepositoryTests
+{
+    [TestFixture]
+    public class AddTests
+    {
+        [Test]
+        public void TestAdd_ShouldCallDbContextSetAdded()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<ILogsDbContext>();
+
+            var repository = new EfGenericRepository<FakeGenericRepositoryType>(mockedDbContext.Object);
+
+            var entity = new Mock<FakeGenericRepositoryType>();
+
+            // Act
+            repository.Add(entity.Object);
+
+            // Assert
+            mockedDbContext.Verify(c => c.SetAdded(entity.Object), Times.Once);
+        }
+    }
+}
