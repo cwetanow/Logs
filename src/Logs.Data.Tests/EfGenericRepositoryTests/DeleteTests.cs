e49764b8a4443dcaef680429bbf7fa3b857ac12e
Ivan <madritistaaa@gmail.com>
2017-03-02 14:30:22 +0200

    Repository delete tests

---
+++ src/Logs.Data.Tests/EfGenericRepositoryTests/DeleteTests.cs
@@ -0,0 +1,28 @@
+﻿using Logs.Data.Contracts;
+using Logs.Data.Tests.Fakes;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Data.Tests.EfGenericRepositoryTests
+{
+    [TestFixture]
+    public class DeleteTests
+    {
+        [Test]
+        public void TestAdd_ShouldCallDbContextSetDeleted()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<ILogsDbContext>();
+
+            var repository = new EfGenericRepository<FakeGenericRepositoryType>(mockedDbContext.Object);
+
+            var entity = new Mock<FakeGenericRepositoryType>();
+
+            // Act
+            repository.Delete(entity.Object);
+
+            // Assert
+            mockedDbContext.Verify(c => c.SetDeleted(entity.Object), Times.Once);
+        }
+    }
+}
