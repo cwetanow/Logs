525697f09fa922ebe82c3a8d2a8ad149eba1ca53
Ivan <madritistaaa@gmail.com>
2017-03-03 15:01:55 +0200

    Add entry service

---
+++ src/Logs.Services/Contracts/IEntryService.cs
@@ -0,0 +1,9 @@
+﻿using Logs.Models;
+
+namespace Logs.Services.Contracts
+{
+    public interface IEntryService
+    {
+        LogEntry CreateNewEntry(string content, int logId);
+    }
+}
---
+++ src/Logs.Services/EntryService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Logs.Data.Contracts;
+using Logs.Models;
+using Logs.Services.Contracts;
+
+namespace Logs.Services
+{
+    public class EntryService : IEntryService
+    {
+        private readonly IRepository<LogEntry> entryRepository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly ILogService logService;
+
+        public EntryService(IRepository<LogEntry> entryRepository,
+            IUnitOfWork unitOfWork, ILogService logService)
+        {
+            if (entryRepository == null)
+            {
+                throw new ArgumentNullException("entryRepository");
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("unitOfWork");
+            }
+
+            this.entryRepository = entryRepository;
+            this.unitOfWork = unitOfWork;
+            this.logService = logService;
+        }
+
+        public LogEntry CreateNewEntry(string content, int logId)
+        {
+            var entry = new LogEntry { Content = content, LogId = logId, EntryDate = DateTime.Now };
+
+            var log = this.logService.GetTrainingLogById(logId);
+            log?.Entries.Add(entry);
+
+            this.entryRepository.Add(entry);
+            this.unitOfWork.Commit();
+
+            return entry;
+        }
+    }
+}
