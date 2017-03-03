67833fc0d28a147da1db8d446fbf9d31e9c9617e
Ivan <madritistaaa@gmail.com>
2017-03-03 15:19:48 +0200

    Add log entry factory

---
+++ src/Logs.Factories/ILogEntryFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Logs.Models;
+
+namespace Logs.Factories
+{
+    public interface ILogEntryFactory
+    {
+        LogEntry CreateLogEntry(string content, DateTime entryDate);
+    }
+}
