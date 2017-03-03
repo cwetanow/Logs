ce8090a8a3ffe0fdd113b69d6b05d429f869c6e1
Ivan <madritistaaa@gmail.com>
2017-03-03 12:26:27 +0200

    Configure route

--- src/Logs.Web/App_Start/RouteConfig.cs
+++ src/Logs.Web/App_Start/RouteConfig.cs
@@ -10,10 +10,15 @@ namespace Logs.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                name: "UserDetails",
+                url: "Profile/Details/{name}",
+                defaults: new { controller = "Profile", action = "Details" });
+
+            routes.MapRoute(
+                 name: "Default",
+                 url: "{controller}/{action}/{id}",
+                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+             );
         }
     }
 }
