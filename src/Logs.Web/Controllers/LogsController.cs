14324654c1f9374272047f5a0082ac34d8da269b
Ivan <madritistaaa@gmail.com>
2017-03-03 14:35:05 +0200

    Beautify

--- src/Logs.Web/Controllers/LogsController.cs
+++ src/Logs.Web/Controllers/LogsController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web.Caching;
 using System.Web.Mvc;
 using Logs.Services.Contracts;
-using Logs.Web.Models;
 using Logs.Web.Models.Logs;
 using Microsoft.AspNet.Identity;
 using PagedList;
