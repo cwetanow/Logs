13102307b994ab7b21b03bb4daf347c1d3c726be
Ivan <madritistaaa@gmail.com>
2017-03-03 23:59:22 +0200

    Add logic for commenting

--- src/Logs.Web/Controllers/EntriesController.cs
+++ src/Logs.Web/Controllers/EntriesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using Logs.Authentication.Contracts;
 using Logs.Models;
 using Logs.Services.Contracts;
 using Logs.Web.Models.Entries;
@@ -13,15 +14,26 @@ namespace Logs.Web.Controllers
     public class EntriesController : Controller
     {
         private readonly IEntryService entryService;
+        private readonly IAuthenticationProvider authenticationProvider;
 
-        public EntriesController(IEntryService entryService)
+        public EntriesController(IEntryService entryService, IAuthenticationProvider authenticationProvider)
         {
             this.entryService = entryService;
+            this.authenticationProvider = authenticationProvider;
         }
 
         public ActionResult NewEntry(NewEntryViewModel model)
         {
             this.entryService.AddEntryToLog(model.Content, model.LogId);
+
+            return null;
+        }
+
+        public ActionResult Comment(NewCommentViewModel model)
+        {
+            model.UserId = authenticationProvider.CurrentUserId;
+
+            this.entryService.AddCommentToLog(model.Content, model.LogId, model.UserId);
 
             return null;
         }
