a8a3fb1146eb37f9c5df03f921a8032d8d767263
Ivan <madritistaaa@gmail.com>
2017-03-03 23:57:52 +0200

    Add view model for commenting

---
+++ src/Logs.Web/Models/Entries/NewCommentViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Logs.Web.Models.Entries
+{
+    public class NewCommentViewModel
+    {
+        public string Content { get; set; }
+
+        public int LogId { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
