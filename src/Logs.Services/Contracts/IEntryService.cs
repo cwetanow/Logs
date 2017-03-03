c6e1724d579dcb697c0950e8513c4e6cdb9ea553
Ivan <madritistaaa@gmail.com>
2017-03-03 23:58:54 +0200

    Add method to entry service

--- src/Logs.Services/Contracts/IEntryService.cs
+++ src/Logs.Services/Contracts/IEntryService.cs
@@ -5,5 +5,7 @@ namespace Logs.Services.Contracts
     public interface IEntryService
     {
         void AddEntryToLog(string content, int logId);
+
+        void AddCommentToLog(string content, int logId, string userId);
     }
 }
--- src/Logs.Services/EntryService.cs
+++ src/Logs.Services/EntryService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Logs.Factories;
+using Logs.Models;
 using Logs.Providers.Contracts;
 using Logs.Services.Contracts;
 
@@ -10,12 +11,21 @@ namespace Logs.Services
         private readonly ILogService logService;
         private readonly IDateTimeProvider dateTimeProvider;
         private readonly ILogEntryFactory factory;
+        private readonly IUserService userService;
 
-        public EntryService(ILogService logService, IDateTimeProvider dateTimeProvider, ILogEntryFactory factory)
+        public EntryService(ILogService logService,
+            IDateTimeProvider dateTimeProvider,
+            ILogEntryFactory factory,
+            IUserService userService)
         {
             if (logService == null)
             {
                 throw new ArgumentNullException("logService");
+            }
+
+            if (userService == null)
+            {
+                throw new ArgumentNullException("userService");
             }
 
             if (dateTimeProvider == null)
@@ -31,6 +41,7 @@ namespace Logs.Services
             this.logService = logService;
             this.factory = factory;
             this.dateTimeProvider = dateTimeProvider;
+            this.userService = userService;
         }
 
         public void AddEntryToLog(string content, int logId)
@@ -40,6 +51,17 @@ namespace Logs.Services
             var entry = this.factory.CreateLogEntry(content, currentDate);
 
             this.logService.AddEntryToLog(logId, entry);
+        }
+
+        public void AddCommentToLog(string content, int logId, string userId)
+        {
+            var currentDate = this.dateTimeProvider.GetCurrenTime();
+
+            var user = this.userService.GetUserById(userId);
+
+            var comment = new Comment { Content = content, Date = currentDate, User = user };
+
+            this.logService.AddCommentToLog(logId, comment);
         }
     }
 }
