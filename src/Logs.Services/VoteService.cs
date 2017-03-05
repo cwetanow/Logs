5b61a6ae99a3db1500c4d569d9c4b0d7d8894741
Ivan <madritistaaa@gmail.com>
2017-03-05 16:54:29 +0200

    Constructor injection to vote servcie

--- src/Logs.Services/VoteService.cs
+++ src/Logs.Services/VoteService.cs
@@ -1,9 +1,26 @@
-﻿using Logs.Services.Contracts;
+﻿using Logs.Data.Contracts;
+using Logs.Factories;
+using Logs.Models;
+using Logs.Services.Contracts;
 
 namespace Logs.Services
 {
     public class VoteService : IVoteService
     {
+        private readonly IRepository<Vote> voteRepository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IVoteFactory voteFactory;
+        private readonly ILogService logService;
+
+        public VoteService(IRepository<Vote> voteRepository, IUnitOfWork unitOfWork, ILogService logService, IVoteFactory voteFactory)
+        {
+            // TODO: Null checks
+            this.voteRepository = voteRepository;
+            this.unitOfWork = unitOfWork;
+            this.logService = logService;
+            this.voteFactory = voteFactory;
+        }
+
         public bool VoteLog(int logId, string userId)
         {
             throw new System.NotImplementedException();
