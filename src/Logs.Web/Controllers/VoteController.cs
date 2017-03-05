2fd615c4e5f6cb63fa6cc6b8c2046a5899fb0494
Ivan <madritistaaa@gmail.com>
2017-03-05 16:47:52 +0200

    Add vote controller with vote action

---
+++ src/Logs.Web/Controllers/VoteController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Logs.Authentication.Contracts;
+using Logs.Services.Contracts;
+
+namespace Logs.Web.Controllers
+{
+    public class VoteController : Controller
+    {
+        private readonly IVoteService voteService;
+        private readonly IAuthenticationProvider authenticationProvider;
+
+        public VoteController(IVoteService voteService, IAuthenticationProvider authenticationProvider)
+        {
+            this.voteService = voteService;
+            this.authenticationProvider = authenticationProvider;
+        }
+        
+        [Authorize]
+        public ActionResult Vote(int logId)
+        {
+            var currentUserId = this.authenticationProvider.CurrentUserId;
+
+            this.voteService.VoteLog(logId, currentUserId);
+
+            return this.RedirectToAction("Details", "Logs", new { id = logId });
+        }
+    }
+}
