74a4e209b9659b5ea0f4ec4e088a558d6424c506
Ivan <madritistaaa@gmail.com>
2017-03-05 16:49:16 +0200

    Add vote service implementation

---
+++ src/Logs.Services/VoteService.cs
@@ -0,0 +1,12 @@
+﻿using Logs.Services.Contracts;
+
+namespace Logs.Services
+{
+    public class VoteService : IVoteService
+    {
+        public bool VoteLog(int logId, string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
