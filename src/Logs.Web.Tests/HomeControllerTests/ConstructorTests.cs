2d5b9efdc8621dd4a64fa66bc909aaedaccd2a94
Ivan <madritistaaa@gmail.com>
2017-03-06 20:35:09 +0200

    Home controller constructor tests

---
+++ src/Logs.Web.Tests/HomeControllerTests/ConstructorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Logs.Authentication.Contracts;
+using Logs.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Web.Tests.HomeControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange, Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new HomeController(null));
+        }
+
+        [Test]
+        public void TestConstructor_PassProviderCorrectly_ShouldInitalizeCorrectly()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            // Act
+            var controller = new HomeController(mockedProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+    }
+}
