6ed02e616313d1da2f2a5e1fa74f70c87a5b146a
Ivan <madritistaaa@gmail.com>
2017-03-06 20:40:01 +0200

    Fix home controller tests

--- src/Logs.Web.Tests/HomeControllerTests/ConstructorTests.cs
+++ src/Logs.Web.Tests/HomeControllerTests/ConstructorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Logs.Authentication.Contracts;
 using Logs.Web.Controllers;
+using Logs.Web.Infrastructure.Factories;
 using Moq;
 using NUnit.Framework;
 
@@ -12,18 +13,32 @@ namespace Logs.Web.Tests.HomeControllerTests
         [Test]
         public void TestConstructor_PassProviderNull_ShouldThrowArgumentNullException()
         {
-            // Arrange, Act, Assert
-            Assert.Throws<ArgumentNullException>(() => new HomeController(null));
+            // Arrange
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new HomeController(null, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassFactoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new HomeController(mockedProvider.Object, null));
         }
 
         [Test]
         public void TestConstructor_PassProviderCorrectly_ShouldInitalizeCorrectly()
         {
             // Arrange
+            var mockedFactory = new Mock<IViewModelFactory>();
             var mockedProvider = new Mock<IAuthenticationProvider>();
 
             // Act
-            var controller = new HomeController(mockedProvider.Object);
+            var controller = new HomeController(mockedProvider.Object, mockedFactory.Object);
 
             // Assert
             Assert.IsNotNull(controller);
