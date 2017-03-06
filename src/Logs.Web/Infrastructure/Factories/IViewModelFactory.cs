3275aa1bee1ff7e32357b43c282b4f555eab1df0
Ivan <madritistaaa@gmail.com>
2017-03-06 20:39:28 +0200

    Add view model factory interface

---
+++ src/Logs.Web/Infrastructure/Factories/IViewModelFactory.cs
@@ -0,0 +1,9 @@
+﻿using Logs.Web.Models.Home;
+
+namespace Logs.Web.Infrastructure.Factories
+{
+    public interface IViewModelFactory
+    {
+        HomeViewModel CreateHomeViewModel(bool isAuthenticated);
+    }
+}
