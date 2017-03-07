82e3d5d160ab11e2894e3aff3a18df04dd98ec1c
Ivan <madritistaaa@gmail.com>
2017-03-07 11:36:27 +0200

    Entries controller constructor tests

---
+++ src/Logs.Web.Tests/EntriesControllerTests/ConstructorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Logs.Authentication.Contracts;
+using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Web.Tests.EntriesControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedService = new Mock<IEntryService>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            // Act
+            var controller = new EntriesController(mockedService.Object, mockedAuthenticationProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void TestConstructor_PassServiceNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new EntriesController(null, mockedAuthenticationProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedService = new Mock<IEntryService>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new EntriesController(mockedService.Object, null));
+        }
+    }
+}
