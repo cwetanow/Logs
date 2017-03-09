42d5e395a2de7123b3223b2b9d7ecbd155fb1566
Ivan <madritistaaa@gmail.com>
2017-03-09 12:53:15 +0200

    Add test for new url

--- src/Logs.Web.Tests/AccountControllerTests/PostLoginTests.cs
+++ src/Logs.Web.Tests/AccountControllerTests/PostLoginTests.cs
@@ -103,6 +103,41 @@ namespace Logs.Web.Tests.AccountControllerTests
             Assert.AreEqual(returnUrl, result.Url);
         }
 
+        [TestCase("pesho@pesho.com", "1234qwerty", true, null)]
+        [TestCase("pesho@pesho.com", "1234qwerty", false, null)]
+        public void TestPostLogin_SuccessReturnUrlIsEmty_ShouldReturnRedirectResultWithHomeIndex(string email,
+           string password,
+           bool remember,
+           string returnUrl)
+        {
+            // Arrange
+            var expectedUrl = "/Home/Index";
+
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            mockedProvider.Setup(
+                    p =>
+                        p.SignInWithPassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>(),
+                            It.IsAny<bool>()))
+                .Returns(SignInStatus.Success);
+
+            var mockedFactory = new Mock<IUserFactory>();
+
+            var model = new LoginViewModel()
+            {
+                Email = email,
+                Password = password,
+                RememberMe = remember
+            };
+
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            // Act
+            var result = controller.Login(model, returnUrl) as RedirectResult;
+
+            // Assert
+            Assert.AreEqual(expectedUrl, result.Url);
+        }
+
         [TestCase("pesho@pesho.com", "1234qwerty", true, "url")]
         [TestCase("pesho@pesho.com", "1234qwerty", false, "return url")]
         public void TestPostLogin_ProviderReturnsLockedOut_ShouldReturnLockoutView(string email,
