defefc698db0214cad6cb010d65d20122b5d5e08
Ivan <madritistaaa@gmail.com>
2017-03-09 12:05:10 +0200

    Fix tests

--- src/Logs.Web.Tests/LogsControllerTests/ConstructorTests.cs
+++ src/Logs.Web.Tests/LogsControllerTests/ConstructorTests.cs
@@ -2,6 +2,7 @@
 using Logs.Authentication.Contracts;
 using Logs.Services.Contracts;
 using Logs.Web.Controllers;
+using Logs.Web.Infrastructure.Factories;
 using Moq;
 using NUnit.Framework;
 
@@ -16,9 +17,10 @@ namespace Logs.Web.Tests.LogsControllerTests
             // Arrange
             var mockedLogService = new Mock<ILogService>();
             var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IViewModelFactory>();
 
             // Act
-            var controller = new LogsController(mockedLogService.Object, mockedAuthenticationProvider.Object);
+            var controller = new LogsController(mockedLogService.Object, mockedAuthenticationProvider.Object, mockedFactory.Object);
 
             // Assert
             Assert.IsNotNull(controller);
@@ -28,20 +30,33 @@ namespace Logs.Web.Tests.LogsControllerTests
         public void TestConstructor_PassLogServiceNull_ShouldThrowArgumentNullException()
         {
             // Arrange
+            var mockedFactory = new Mock<IViewModelFactory>();
             var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
 
             // Act, Assert
-            Assert.Throws<ArgumentNullException>(() => new LogsController(null, mockedAuthenticationProvider.Object));
+            Assert.Throws<ArgumentNullException>(() => new LogsController(null, mockedAuthenticationProvider.Object, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassFactoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedLogService = new Mock<ILogService>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new LogsController(mockedLogService.Object, mockedAuthenticationProvider.Object, null));
         }
 
         [Test]
         public void TestConstructor_PassAuthenticationProviderNull_ShouldThrowArgumentNullException()
         {
             // Arrange
+            var mockedFactory = new Mock<IViewModelFactory>();
             var mockedLogService = new Mock<ILogService>();
 
             // Act, Assert
-            Assert.Throws<ArgumentNullException>(() => new LogsController(mockedLogService.Object, null));
+            Assert.Throws<ArgumentNullException>(() => new LogsController(mockedLogService.Object, null, mockedFactory.Object));
         }
     }
 }
