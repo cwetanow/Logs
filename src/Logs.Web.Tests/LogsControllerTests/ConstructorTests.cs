633696b1877006a25cb3ea1b92cc0031213f580e
Ivan <madritistaaa@gmail.com>
2017-03-09 11:59:35 +0200

    Logs controller constructor tests

---
+++ src/Logs.Web.Tests/LogsControllerTests/ConstructorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Logs.Authentication.Contracts;
+using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Web.Tests.LogsControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedLogService = new Mock<ILogService>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            // Act
+            var controller = new LogsController(mockedLogService.Object, mockedAuthenticationProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void TestConstructor_PassLogServiceNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new LogsController(null, mockedAuthenticationProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassAuthenticationProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedLogService = new Mock<ILogService>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new LogsController(mockedLogService.Object, null));
+        }
+    }
+}
