b902ebacf5fe3c3ae572545fa405fb72f7121c20
Ivan <madritistaaa@gmail.com>
2017-03-09 12:50:18 +0200

    Check return url

--- src/Logs.Web/Controllers/AccountController.cs
+++ src/Logs.Web/Controllers/AccountController.cs
@@ -53,6 +53,8 @@ namespace Logs.Web.Controllers
                 return this.View(model);
             }
 
+            returnUrl = string.IsNullOrEmpty(returnUrl) ? "/Home/Index" : returnUrl;
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = this.provider.SignInWithPassword(model.Email, model.Password, model.RememberMe, shouldLockout: false);
