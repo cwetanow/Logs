555cce6817511562a53795ab51b509a6ba6377bd
Ivan <madritistaaa@gmail.com>
2017-03-09 19:33:17 +0200

    Vote model tests

---
+++ src/Logs.Models.Tests/VoteTests/PropertiesTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Logs.Models.Tests.VoteTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [TestCase(1)]
+        [TestCase(423)]
+        [TestCase(123)]
+        public void TestLogVoteId_ShouldInitializeCorrectly(int voteId)
+        {
+            // Arrange
+            var vote = new Vote();
+
+            // Act
+            vote.LogVoteId = voteId;
+
+            // Assert
+            Assert.AreEqual(voteId, vote.LogVoteId);
+        }
+    }
+}
