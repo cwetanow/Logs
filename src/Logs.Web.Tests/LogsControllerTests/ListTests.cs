004e0a23ecd96bfc5756b0f3a65ad2e4f5c16f6b
Ivan <madritistaaa@gmail.com>
2017-03-11 14:00:38 +0200

    List tests

---
+++ src/Logs.Web.Tests/LogsControllerTests/ListTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Logs.Authentication.Contracts;
+using Logs.Models;
+using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Logs.Web.Infrastructure.Factories;
+using Logs.Web.Models.Logs;
+using Moq;
+using NUnit.Framework;
+using PagedList;
+
+namespace Logs.Web.Tests.LogsControllerTests
+{
+    [TestFixture]
+    public class ListTests
+    {
+        [Test]
+        public void TestList_ShouldReturnCorrectView()
+        {
+            // Arrange
+            var expectedViewName = "List";
+
+            var mockedLogService = new Mock<ILogService>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var controller = new LogsController(mockedLogService.Object, mockedAuthenticationProvider.Object,
+                mockedFactory.Object);
+
+            // Act
+            var result = controller.List();
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+    }
+}
