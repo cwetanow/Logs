d734048d1d087080808a56b7c53d86874ddbfbab
Ivan <madritistaaa@gmail.com>
2017-03-11 15:51:18 +0200

    Add caching provider

---
+++ src/Logs.Providers/Contracts/ICachingProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Logs.Providers.Contracts
+{
+    public interface ICachingProvider
+    {
+        void AddItem(string key, object item);
+
+        object GetItem(string key);
+
+        void RemoveItem(string key);
+    }
+}
---
+++ src/Logs.Providers/HttpContextCachingProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+using Logs.Providers.Contracts;
+
+namespace Logs.Providers
+{
+    public class HttpContextCachingProvider : ICachingProvider
+    {
+        protected Cache Cache => HttpContext.Current.Cache;
+
+        public void AddItem(string key, object item)
+        {
+            this.Cache[key] = item;
+        }
+
+        public object GetItem(string key)
+        {
+            return this.Cache.Get(key);
+        }
+
+        public void RemoveItem(string key)
+        {
+            this.Cache.Remove(key);
+        }
+    }
+}
