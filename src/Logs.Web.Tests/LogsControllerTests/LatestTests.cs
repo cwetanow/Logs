250662edc811cd15bd4b4a1c1b74c70848068142
Ivan <madritistaaa@gmail.com>
2017-03-11 13:26:09 +0200

    Finish latest tests

--- src/Logs.Web.Tests/LogsControllerTests/LatestTests.cs
+++ src/Logs.Web.Tests/LogsControllerTests/LatestTests.cs
@@ -1,8 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Logs.Authentication.Contracts;
+using Logs.Models;
 using Logs.Services.Contracts;
 using Logs.Web.Controllers;
 using Logs.Web.Infrastructure.Factories;
+using Logs.Web.Models.Logs;
 using Moq;
 using NUnit.Framework;
 
@@ -49,6 +52,34 @@ namespace Logs.Web.Tests.LogsControllerTests
 
             // Assert
             Assert.AreEqual(expectedViewName, result.ViewName);
+        }
+
+        [TestCase(1)]
+        [TestCase(423)]
+        public void TestLatest_PassCount_ShoudSetCorrectModel(int count)
+        {
+            // Assert
+            var logs = new List<TrainingLog> { new TrainingLog() };
+
+            var mockedLogService = new Mock<ILogService>();
+            mockedLogService.Setup(s => s.GetLatestLogs(It.IsAny<int>())).Returns(logs);
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var viewModel = new ShortLogViewModel();
+            mockedFactory.Setup(f => f.CreateShortLogViewModel(It.IsAny<TrainingLog>())).Returns(viewModel);
+
+            var model = new List<ShortLogViewModel> { viewModel };
+
+            var controller = new LogsController(mockedLogService.Object, mockedAuthenticationProvider.Object,
+                mockedFactory.Object);
+
+            // Act
+            var result = controller.Latest(count) as PartialViewResult;
+
+            // Assert
+            CollectionAssert.AreEqual(model, (IEnumerable<ShortLogViewModel>)result.Model);
         }
     }
 }
