1095519ad718d203a9acd52a6bd97fd9cef86603
Ivan <madritistaaa@gmail.com>
2017-03-12 11:48:26 +0200

    Add interceptor

---
+++ src/Logs.Web/Infrastructure/Interceptors/CachingInterceptor.cs
@@ -0,0 +1,12 @@
+﻿using Ninject.Extensions.Interception;
+
+namespace Logs.Web.Infrastructure.Interceptors
+{
+    public class CachingInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            // TODO: Write logic
+        }
+    }
+}
