4714d4e453b7a3b1193e37e0d49ac440cd95aceb
Ivan <madritistaaa@gmail.com>
2017-03-13 21:39:31 +0200

    Start details tests

---
+++ src/Logs.Web.Tests/ProfileControllerTests/DetailsTests.cs
@@ -0,0 +1,33 @@
+﻿using Logs.Authentication.Contracts;
+using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Web.Tests.ProfileControllerTests
+{
+    [TestFixture]
+    public class DetailsTests
+    {
+        [TestCase("username")]
+        [TestCase("my-username")]
+        [TestCase("lalalala96")]
+        [TestCase("muchwow")]
+        [TestCase("meBeUser")]
+        [TestCase("IAMVERYGOOD")]
+        public void TestDetails_ShouldCallServiceGetByUsername(string username)
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedService = new Mock<IUserService>();
+
+            var controller = new ProfileController(mockedProvider.Object, mockedService.Object);
+
+            // Act
+            controller.Details(username);
+
+            // Assert
+            mockedService.Verify(s => s.GetUserByUsername(username), Times.Once);
+        }
+    }
+}
