a945f78f8dffd5409217ebfaf41c270e62fb7af1
Ivan <madritistaaa@gmail.com>
2017-03-13 13:35:12 +0200

    Add caching provider

---
+++ src/Logs.Providers/HttpContextCachingProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+using Logs.Providers.Contracts;
+
+namespace Logs.Providers
+{
+    public class HttpContextCachingProvider : ICachingProvider
+    {
+        protected Cache Cache => HttpContext.Current.Cache;
+
+        public void AddItem(string key, object item, DateTime expirationDateTime)
+        {
+            this.Cache.Add(key, item, null, expirationDateTime, Cache.NoSlidingExpiration, CacheItemPriority.Default,
+                null);
+        }
+
+        public object GetItem(string key)
+        {
+            return this.Cache.Get(key);
+        }
+    }
+}
