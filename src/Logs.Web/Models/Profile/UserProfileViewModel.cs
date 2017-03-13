a7aba7a433e34c4c17a321def68d4db3c5b9c607
Ivan <madritistaaa@gmail.com>
2017-03-13 21:05:19 +0200

    Add user profile view model

---
+++ src/Logs.Web/Models/Profile/UserProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Logs.Web.Models.Profile
+{
+    public class UserProfileViewModel
+    {
+        public string Gender { get; set; }
+
+        public double Weight { get; set; }
+
+        public int Age { get; set; }
+
+        public double BodyFatPercent { get; set; }
+
+        public int Height { get; set; }
+
+        public string Description { get; set; }
+    }
+}
