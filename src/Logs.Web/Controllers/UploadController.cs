915ee9314f50d8351b6e36e4559e4a98b4e893b1
Ivan <madritistaaa@gmail.com>
2017-03-14 13:36:20 +0200

    Add upload controller

---
+++ src/Logs.Web/Controllers/UploadController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Logs.Web.Models.Upload;
+
+namespace Logs.Web.Controllers
+{
+    public class UploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View(new UploadViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(UploadViewModel model)
+        {
+            return null;
+        }
+    }
+}
