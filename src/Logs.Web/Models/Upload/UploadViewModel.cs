920d6080a023f2f18949a61d7bad011bcc322979
Ivan <madritistaaa@gmail.com>
2017-03-14 13:36:32 +0200

    Upload view model

---
+++ src/Logs.Web/Models/Upload/UploadViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CloudinaryDotNet;
+
+namespace Logs.Web.Models.Upload
+{
+    public class UploadViewModel
+    {
+        public UploadViewModel()
+        {
+            var account = new CloudinaryDotNet.Account(
+  "cwetanow",
+  "742665798753294",
+  "7dLDYfT_LfNCGI9FPtyEG7G8dm0");
+
+            this.Cloudinary = new Cloudinary(account);
+        }
+
+        public Cloudinary Cloudinary { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
