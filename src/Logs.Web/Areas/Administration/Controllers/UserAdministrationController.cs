973d237aa2c211e4b6a8f90c1deba0bf15a8a92c
Ivan <madritistaaa@gmail.com>
2017-03-15 11:09:12 +0200

    User administration controller

---
+++ src/Logs.Web/Areas/Administration/Controllers/UserAdministrationController.cs
@@ -0,0 +1,57 @@
+﻿using System.Web.Mvc;
+
+namespace Logs.Web.Areas.Administration.Controllers
+{
+    [Authorize(Roles = "administrator")]
+    public class UserAdministrationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        // GET: Administration/UserAdministration/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Administration/UserAdministration/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Administration/UserAdministration/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Administration/UserAdministration/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
