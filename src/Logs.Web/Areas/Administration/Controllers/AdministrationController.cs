ac744770213eac3f4cb38221b96130aba7e9c28c
Ivan <madritistaaa@gmail.com>
2017-03-15 10:30:24 +0200

    Add administration area

---
+++ src/Logs.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Logs.Web.Areas.Administration.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
