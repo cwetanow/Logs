fc8fe18a951e6ea20ed0642e09ab5522dd12fff7
Ivan <madritistaaa@gmail.com>
2017-03-15 23:18:23 +0200

    Remove methods from user admin controller

--- src/Logs.Web/Areas/Administration/Controllers/UserAdministrationController.cs
+++ src/Logs.Web/Areas/Administration/Controllers/UserAdministrationController.cs
@@ -47,49 +47,5 @@ namespace Logs.Web.Areas.Administration.Controllers
 
             return this.View(model.ToPagedList(page, count));
         }
-
-        // GET: Administration/UserAdministration/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Administration/UserAdministration/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Administration/UserAdministration/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Administration/UserAdministration/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
