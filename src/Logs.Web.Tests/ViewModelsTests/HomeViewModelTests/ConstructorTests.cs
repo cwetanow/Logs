358af78dbe1e1bc7bd4b70313661c6747e7a94f0
Ivan <madritistaaa@gmail.com>
2017-03-16 19:07:25 +0200

    Home vm tests

---
+++ src/Logs.Web.Tests/ViewModelsTests/HomeViewModelTests/ConstructorTests.cs
@@ -0,0 +1,31 @@
+﻿using Logs.Web.Models.Home;
+using NUnit.Framework;
+
+namespace Logs.Web.Tests.ViewModelsTests.HomeViewModelTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public void TestConstructor_ShouldInitializeCorrectly(bool isAuthenticated)
+        {
+            // Arrange, Act
+            var model = new HomeViewModel(isAuthenticated);
+
+            // Act
+            Assert.IsNotNull(model);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void TestConstructor_ShouldSetIsAuthenticatedCorrectly(bool isAuthenticated)
+        {
+            // Arrange, Act
+            var model = new HomeViewModel(isAuthenticated);
+
+            // Act
+            Assert.AreEqual(isAuthenticated, model.IsAuthenticated);
+        }
+    }
+}
