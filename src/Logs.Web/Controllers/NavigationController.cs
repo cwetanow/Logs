7db37327adb38b494af4c750cd34b6ddc72826af
Ivan <madritistaaa@gmail.com>
2017-03-16 11:47:10 +0200

    Add navigation controller

---
+++ src/Logs.Web/Controllers/NavigationController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Logs.Web.Models.Navigation;
+
+namespace Logs.Web.Controllers
+{
+    public class NavigationController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new NavigationViewModel();
+            return this.PartialView("Navigation", model);
+        }
+    }
+}
---
+++ src/Logs.Web/Models/Navigation/NavigationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Logs.Web.Models.Navigation
+{
+    public class NavigationViewModel
+    {
+        public bool IsAuthenticated { get; set; }
+
+        public string Username { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
