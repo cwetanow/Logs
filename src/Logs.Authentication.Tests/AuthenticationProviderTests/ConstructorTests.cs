4fbaf011ba662375da5910a6e149c10307c8a389
Ivan <madritistaaa@gmail.com>
2017-03-18 12:52:05 +0200

    Auth provider constructor tests

---
+++ src/Logs.Authentication.Tests/AuthenticationProviderTests/ConstructorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Logs.Providers.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Authentication.Tests.AuthenticationProviderTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+
+            // Act
+            var provider = new AuthenticationProvider(mockedDateTimeProvider.Object, mockedHttpContextProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(provider);
+        }
+
+        [Test]
+        public void TestConstructor_PassDateTimeProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new AuthenticationProvider(null, mockedHttpContextProvider.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassHttpContextProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new AuthenticationProvider(mockedDateTimeProvider.Object, null));
+        }
+    }
+}
