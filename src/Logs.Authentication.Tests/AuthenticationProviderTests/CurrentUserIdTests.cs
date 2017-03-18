99bd4340f8bd0da38729a1d80a3674fb5ff42def
Ivan <madritistaaa@gmail.com>
2017-03-18 13:26:28 +0200

    More properties tests

---
+++ src/Logs.Authentication.Tests/AuthenticationProviderTests/CurrentUserIdTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Principal;
+using Logs.Providers.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Authentication.Tests.AuthenticationProviderTests
+{
+    [TestFixture]
+    public class CurrentUserIdTests
+    {
+        [Test]
+        public void TestCurrentUserId_ShouldCallHttpContextProviderCurrentIdentity()
+        {
+            // Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var mockedIdentity = new Mock<IIdentity>();
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.CurrentIdentity).Returns(mockedIdentity.Object);
+
+            var provider = new AuthenticationProvider(mockedDateTimeProvider.Object, mockedHttpContextProvider.Object);
+
+            // Act
+            var result = provider.CurrentUserId;
+
+            // Assert
+            mockedHttpContextProvider.Verify(p => p.CurrentIdentity, Times.Once);
+        }
+    }
+}
---
+++ src/Logs.Authentication.Tests/AuthenticationProviderTests/CurrentUserUsernameTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Principal;
+using Logs.Providers.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Authentication.Tests.AuthenticationProviderTests
+{
+    [TestFixture]
+    public class CurrentUserUsernameTests
+    {
+        [Test]
+        public void TestCurrentUserUsername_ShouldCallHttpContextProviderCurrentIdentity()
+        {
+            // Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var mockedIdentity = new Mock<IIdentity>();
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.CurrentIdentity).Returns(mockedIdentity.Object);
+
+            var provider = new AuthenticationProvider(mockedDateTimeProvider.Object, mockedHttpContextProvider.Object);
+
+            // Act
+            var result = provider.CurrentUserUsername;
+
+            // Assert
+            mockedHttpContextProvider.Verify(p => p.CurrentIdentity, Times.Once);
+        }
+    }
+}
