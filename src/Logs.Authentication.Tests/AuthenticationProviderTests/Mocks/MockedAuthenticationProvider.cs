7ad57a5550c30605672861921256b06cd409985b
Ivan <madritistaaa@gmail.com>
2017-03-18 13:09:57 +0200

    Remove props from mock type

--- src/Logs.Authentication.Tests/AuthenticationProviderTests/Mocks/MockedAuthenticationProvider.cs
+++ src/Logs.Authentication.Tests/AuthenticationProviderTests/Mocks/MockedAuthenticationProvider.cs
@@ -1,7 +1,5 @@
-﻿using System.Security.Principal;
-using Logs.Authentication.Managers;
+﻿using Logs.Authentication.Managers;
 using Logs.Providers.Contracts;
-using Microsoft.Owin;
 
 namespace Logs.Authentication.Tests.AuthenticationProviderTests.Mocks
 {
@@ -10,16 +8,6 @@ namespace Logs.Authentication.Tests.AuthenticationProviderTests.Mocks
         public MockedAuthenticationProvider(IDateTimeProvider dateTimeProvider, IHttpContextProvider httpContextProvider)
             : base(dateTimeProvider, httpContextProvider)
         {
-        }
-
-        public IOwinContext GetOwinContext()
-        {
-            return this.OwinContext;
-        }
-
-        public IIdentity GetIdentity()
-        {
-            return this.Identity;
         }
 
         public ApplicationSignInManager GetSignInManager()
