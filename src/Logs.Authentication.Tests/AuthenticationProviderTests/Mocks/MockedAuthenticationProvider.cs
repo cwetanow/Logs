15b5dc5c78591ad8efe84bd8e74f6d49214510f6
Ivan <madritistaaa@gmail.com>
2017-03-18 13:00:52 +0200

    Add mocked auth provider exposing protected properties

---
+++ src/Logs.Authentication.Tests/AuthenticationProviderTests/Mocks/MockedAuthenticationProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Principal;
+using Logs.Authentication.Managers;
+using Logs.Providers.Contracts;
+using Microsoft.Owin;
+
+namespace Logs.Authentication.Tests.AuthenticationProviderTests.Mocks
+{
+    public class MockedAuthenticationProvider : AuthenticationProvider
+    {
+        public MockedAuthenticationProvider(IDateTimeProvider dateTimeProvider, IHttpContextProvider httpContextProvider)
+            : base(dateTimeProvider, httpContextProvider)
+        {
+        }
+
+        public IOwinContext GetOwinContext()
+        {
+            return this.OwinContext;
+        }
+
+        public IIdentity GetIdentity()
+        {
+            return this.Identity;
+        }
+
+        public ApplicationSignInManager GetSignInManager()
+        {
+            return this.SignInManager;
+        }
+
+        public ApplicationUserManager GetUserManager()
+        {
+            return this.UserManager;
+        }
+    }
+}
