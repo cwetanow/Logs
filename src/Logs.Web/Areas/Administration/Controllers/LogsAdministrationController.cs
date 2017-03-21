5c36f81516571143263f57f5be0208a60ca932d0
Ivan <madritistaaa@gmail.com>
2017-03-21 14:18:29 +0200

    Logs admin controller

---
+++ src/Logs.Web/Areas/Administration/Controllers/LogsAdministrationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Logs.Services.Contracts;
+using Logs.Web.Models.Logs;
+using PagedList;
+
+namespace Logs.Web.Areas.Administration.Controllers
+{
+    [Authorize(Roles = AdministratorRoleName)]
+    public class LogsAdministrationController : Controller
+    {
+        private const string AdministratorRoleName = "administrator";
+
+        private readonly ILogService logService;
+
+        public LogsAdministrationController(ILogService logService)
+        {
+            this.logService = logService;
+        }
+
+        public ActionResult Index(int page = 1, int count = 10)
+        {
+            var logs = this.logService.GetAll()
+                .Select(l => new ShortLogViewModel(l))
+                .ToPagedList(page, count);
+
+            return this.View(logs);
+        }
+    }
+}
