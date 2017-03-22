3043662419e49a5ce422d2f97f7250b17e3796e1
Ivan <madritistaaa@gmail.com>
2017-03-22 11:54:34 +0200

    Caching provider constructor tests

---
+++ src/Logs.Providers.Tests/CachingProviderTests/ConstructorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Logs.Providers.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Providers.Tests.CachingProviderTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassEverything_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+
+            // Act
+            var provider = new CachingProvider(mockedHttpContextProvider.Object);
+
+            // Assert
+            Assert.IsNotNull(provider);
+        }
+
+        [Test]
+        public void TestConstructor_ShouldBeInstanceOfICachingProvider()
+        {
+            // Arrange
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+
+            // Act
+            var provider = new CachingProvider(mockedHttpContextProvider.Object);
+
+            // Assert
+            Assert.IsInstanceOf<ICachingProvider>(provider);
+        }
+
+        [Test]
+        public void TestConstructor_PassHttpContextProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange, Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new CachingProvider(null));
+        }
+    }
+}
