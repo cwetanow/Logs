9a853b5d1643e198e838828c14e3ca5df0955b6f
Ivan <madritistaaa@gmail.com>
2017-03-22 12:18:18 +0200

    More caching provider tests

---
+++ src/Logs.Providers.Tests/CachingProviderTests/AddItemTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using Logs.Providers.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Providers.Tests.CachingProviderTests
+{
+    [TestFixture]
+    public class AddItemTests
+    {
+        [TestCase("key")]
+        [TestCase("cachekey")]
+        public void TestAddItem_ShouldCallHttpContextProviderCurrentHttpContext(string key)
+        {
+            // Arrange
+            var cache = HttpRuntime.Cache;
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.ContextCache).Returns(cache);
+
+            var provider = new CachingProvider(mockedHttpContextProvider.Object);
+
+            // Act
+            provider.AddItem(key, new object(), new DateTime());
+
+            // Assert
+            mockedHttpContextProvider.Verify(p => p.ContextCache, Times.Once);
+        }
+    }
+}
---
+++ src/Logs.Providers.Tests/CachingProviderTests/GetItemTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Web;
+using Logs.Providers.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Providers.Tests.CachingProviderTests
+{
+    [TestFixture]
+    public class GetItemTests
+    {
+        [TestCase("key")]
+        [TestCase("cachekey")]
+        public void TestGetItem_ShouldCallHttpContextProviderCurrentCache(string key)
+        {
+            // Arrange
+            var cache = HttpRuntime.Cache;
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.ContextCache).Returns(cache);
+
+            var provider = new CachingProvider(mockedHttpContextProvider.Object);
+
+            // Act
+            provider.GetItem(key);
+
+            // Assert
+            mockedHttpContextProvider.Verify(p => p.ContextCache, Times.Once);
+        }
+
+        [TestCase("key")]
+        [TestCase("cachekey")]
+        public void TestGetItem_ShouldReturnCorrectly(string key)
+        {
+            // Arrange
+            var obj = new object();
+
+            var cache = HttpRuntime.Cache;
+            cache[key] = obj;
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.ContextCache).Returns(cache);
+
+            var provider = new CachingProvider(mockedHttpContextProvider.Object);
+
+            // Act
+            var result = provider.GetItem(key);
+
+            // Assert
+            Assert.AreSame(obj, result);
+        }
+    }
+}
