4681aeb6bc9367e84d4d72f0604f6e83e514e465
Ivan <madritistaaa@gmail.com>
2017-03-23 15:36:23 +0200

    Fix service tests

--- src/Logs.Services.Tests/LogsServiceTests/EditLogDescriptionTests.cs
+++ src/Logs.Services.Tests/LogsServiceTests/EditLogDescriptionTests.cs
@@ -9,11 +9,11 @@ using NUnit.Framework;
 namespace Logs.Services.Tests.LogsServiceTests
 {
     [TestFixture]
-    public class EditLogDescriptionTests
+    public class EditLogTests
     {
-        [TestCase(1, "description")]
-        [TestCase(1423, "another description")]
-        public void TestEditLogDescription_ShouldCallRepositoryGetById(int logId, string newDescription)
+        [TestCase(1, "description", "name")]
+        [TestCase(1423, "another description", "name")]
+        public void TestEditLog_ShouldCallRepositoryGetById(int logId, string newDescription, string newName)
         {
             // Arrange
             var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
@@ -29,15 +29,15 @@ namespace Logs.Services.Tests.LogsServiceTests
                 mockedDateTimeProvider.Object);
 
             // Act
-            service.EditLogDescription(logId, newDescription);
+            service.EditLog(logId, newDescription, newName);
 
             // Assert
             mockedLogRepository.Verify(r => r.GetById(logId), Times.Once);
         }
 
-        [TestCase(1, "description")]
-        [TestCase(1423, "another description")]
-        public void TestEditLogDescription_RepositoryReturnsNull_ShouldNotCallUnitOfWorkCommit(int logId, string newDescription)
+        [TestCase(1, "description", "name")]
+        [TestCase(1423, "another description", "name")]
+        public void TestEditLog_RepositoryReturnsNull_ShouldNotCallUnitOfWorkCommit(int logId, string newDescription, string newName)
         {
             // Arrange
             var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
@@ -53,15 +53,15 @@ namespace Logs.Services.Tests.LogsServiceTests
                 mockedDateTimeProvider.Object);
 
             // Act
-            service.EditLogDescription(logId, newDescription);
+            service.EditLog(logId, newDescription, newName);
 
             // Assert
             mockedUnitOfWork.Verify(u => u.Commit(), Times.Never);
         }
 
-        [TestCase(1, "description")]
-        [TestCase(1423, "another description")]
-        public void TestEditLogDescription_RepositoryReturnsLog_ShouldSetLogDescription(int logId, string newDescription)
+        [TestCase(1, "description", "name")]
+        [TestCase(1423, "another description", "name")]
+        public void TestEditLog_RepositoryReturnsLog_ShouldSetLogDescription(int logId, string newDescription, string newName)
         {
             // Arrange
             var log = new TrainingLog();
@@ -81,15 +81,15 @@ namespace Logs.Services.Tests.LogsServiceTests
                 mockedDateTimeProvider.Object);
 
             // Act
-            service.EditLogDescription(logId, newDescription);
+            service.EditLog(logId, newDescription, newName);
 
             // Assert
             Assert.AreEqual(newDescription, log.Description);
         }
 
-        [TestCase(1, "description")]
-        [TestCase(1423, "another description")]
-        public void TestEditLogDescription_RepositoryReturnsLog_ShouldCallRepositoryUpdate(int logId, string newDescription)
+        [TestCase(1, "description", "name")]
+        [TestCase(1423, "another description", "name")]
+        public void TestEditLog_RepositoryReturnsLog_ShouldSetLogName(int logId, string newDescription, string newName)
         {
             // Arrange
             var log = new TrainingLog();
@@ -109,15 +109,43 @@ namespace Logs.Services.Tests.LogsServiceTests
                 mockedDateTimeProvider.Object);
 
             // Act
-            service.EditLogDescription(logId, newDescription);
+            service.EditLog(logId, newDescription, newName);
+
+            // Assert
+            Assert.AreEqual(newName, log.Name);
+        }
+
+        [TestCase(1, "description", "name")]
+        [TestCase(1423, "another description", "name")]
+        public void TestEditLog_RepositoryReturnsLog_ShouldCallRepositoryUpdate(int logId, string newDescription, string newName)
+        {
+            // Arrange
+            var log = new TrainingLog();
+
+            var mockedLogRepository = new Mock<IRepository<TrainingLog>>();
+            mockedLogRepository.Setup(r => r.GetById(It.IsAny<object>())).Returns(log);
+
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedLogFactory = new Mock<ITrainingLogFactory>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var service = new LogsService(mockedLogRepository.Object,
+                mockedUnitOfWork.Object,
+                mockedLogFactory.Object,
+                mockedUserService.Object,
+                mockedDateTimeProvider.Object);
+
+            // Act
+            service.EditLog(logId, newDescription, newName);
 
             // Assert
             mockedLogRepository.Verify(r => r.Update(log), Times.Once);
         }
 
-        [TestCase(1, "description")]
-        [TestCase(1423, "another description")]
-        public void TestEditLogDescription_RepositoryReturnsLog_ShouldCallUnitOfWorkCommit(int logId, string newDescription)
+        [TestCase(1, "description", "name")]
+        [TestCase(1423, "another description", "name")]
+        public void TestEditLog_RepositoryReturnsLog_ShouldCallUnitOfWorkCommit(int logId, string newDescription, string newName)
         {
             // Arrange
             var log = new TrainingLog();
@@ -137,7 +165,7 @@ namespace Logs.Services.Tests.LogsServiceTests
                 mockedDateTimeProvider.Object);
 
             // Act
-            service.EditLogDescription(logId, newDescription);
+            service.EditLog(logId, newDescription, newName);
 
             // Assert
             mockedUnitOfWork.Verify(r => r.Commit(), Times.Once);
