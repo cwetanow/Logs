43b5b99204dc02749cadf1d047fd59f0eb6b0069
Ivan <madritistaaa@gmail.com>
2017-03-26 12:36:35 +0300

    Add search controller

---
+++ src/Logs.Web/Controllers/SearchController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.Mvc;
+using Logs.Services.Contracts;
+using Logs.Web.Infrastructure.Factories;
+
+namespace Logs.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly ILogService logService;
+        private readonly IViewModelFactory viewModelFactory;
+
+        public SearchController(IUserService userService, ILogService logService, IViewModelFactory viewModelFactory)
+        {
+            if (userService == null)
+            {
+                throw new ArgumentNullException(nameof(userService));
+            }
+
+            if (logService == null)
+            {
+                throw new ArgumentNullException(nameof(logService));
+            }
+
+            if (viewModelFactory == null)
+            {
+                throw new ArgumentNullException(nameof(viewModelFactory));
+            }
+
+            this.logService = logService;
+            this.viewModelFactory = viewModelFactory;
+            this.userService = userService;
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
