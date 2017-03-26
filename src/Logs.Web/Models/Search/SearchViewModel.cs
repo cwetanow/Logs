bfa0c108f8a3dd546118a85a731c196e9a71ed16
Ivan <madritistaaa@gmail.com>
2017-03-26 12:36:42 +0300

    Add search view model

---
+++ src/Logs.Web/Models/Search/SearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Logs.Web.Models.Search
+{
+    public class SearchViewModel
+    {
+        public string SearchTerm { get; set; }
+
+        public bool IsSearchingUsers { get; set; }
+
+        public bool IsSearchingLogs { get; set; }
+    }
+}
