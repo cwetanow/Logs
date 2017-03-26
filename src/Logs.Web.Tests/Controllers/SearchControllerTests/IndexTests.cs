e3d987c0d7a0f4365c0c90d049caca9f853997d0
Ivan <madritistaaa@gmail.com>
2017-03-26 18:32:37 +0300

    Search controller index tests

---
+++ src/Logs.Web.Tests/Controllers/SearchControllerTests/IndexTests.cs
@@ -0,0 +1,28 @@
+﻿using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Logs.Web.Infrastructure.Factories;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace Logs.Web.Tests.Controllers.SearchControllerTests
+{
+    [TestFixture]
+    public class IndexTests
+    {
+        [Test]
+        public void TestIndex_ShouldReturnCorrectView()
+        {
+            // Arrange
+            var mockedLogService = new Mock<ILogService>();
+            var mockedViewModelFactory = new Mock<IViewModelFactory>();
+
+            var controller = new SearchController(mockedLogService.Object, mockedViewModelFactory.Object);
+
+            // Act, Assert
+            controller
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
