d340f9b97987bb20b4b284d11a2e39e078850281
Ivan <madritistaaa@gmail.com>
2017-04-05 23:14:38 +0300

    Add tests for CommitAsync

---
+++ src/Logs.Data.Tests/UnitOfWorkTests/CommitAsyncTests.cs
@@ -0,0 +1,25 @@
+﻿using Logs.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Data.Tests.UnitOfWorkTests
+{
+    [TestFixture]
+    public class CommitAsyncTests
+    {
+        [Test]
+        public void TestCommit_ShouldCallDbContextSaveChangesAsync()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<ILogsDbContext>();
+
+            var unitOfWork = new UnitOfWork(mockedDbContext.Object);
+
+            // Act
+            unitOfWork.CommitAsync();
+
+            // Assert
+            mockedDbContext.Verify(c => c.SaveChangesAsync(), Times.Once);
+        }
+    }
+}
