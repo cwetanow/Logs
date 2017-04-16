8e843ff81e8d3836e2fa1670269dd7d38ee35ec7
Ivan <madritistaaa@gmail.com>
2017-04-16 22:39:46 +0300

    Add temp date error attributes

---
+++ src/Logs.Web/Infrastructure/Attributes/RestoreModelStateFromTempDataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Logs.Web.Infrastructure.Attributes
+{
+    public class RestoreModelStateFromTempDataAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            if (filterContext.Controller.TempData.ContainsKey("ModelState"))
+            {
+                filterContext.Controller.ViewData.ModelState.Merge(
+                    (ModelStateDictionary)filterContext.Controller.TempData["ModelState"]);
+            }
+        }
+    }
+}
---
+++ src/Logs.Web/Infrastructure/Attributes/SetTempDataModelStateAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Logs.Web.Infrastructure.Attributes
+{
+    public class SetTempDataModelStateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            filterContext.Controller.TempData["ModelState"] = filterContext.Controller.ViewData.ModelState;
+        }
+    }
+}
