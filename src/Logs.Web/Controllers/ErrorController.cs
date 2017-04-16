b3315066c5d14e0f1d689ece039d8773d6dba6d9
Ivan <madritistaaa@gmail.com>
2017-04-16 10:00:34 +0300

    Cache error pages

--- src/Logs.Web/Controllers/ErrorController.cs
+++ src/Logs.Web/Controllers/ErrorController.cs
@@ -5,12 +5,14 @@ namespace Logs.Web.Controllers
     public class ErrorController : Controller
     {
         [HttpGet]
+        [OutputCache(Duration = 60 * 60 * 24)]
         public ActionResult NotFound()
         {
             return this.View();
         }
 
         [HttpGet]
+        [OutputCache(Duration = 60 * 60 * 24)]
         public ActionResult ServerError()
         {
             return this.View();
