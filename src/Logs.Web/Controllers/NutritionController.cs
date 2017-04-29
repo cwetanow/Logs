cc8de44dea67bdf29d23279033ba1ba82a3423bb
Ivan <madritistaaa@gmail.com>
2017-04-29 22:47:50 +0300

    Add nutrition controller

---
+++ src/Logs.Web/Controllers/NutritionController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Logs.Services.Contracts;
+
+namespace Logs.Web.Controllers
+{
+    public class NutritionController : Controller
+    {
+        private readonly INutritionService nutritionService;
+
+        public NutritionController(INutritionService nutritionService)
+        {
+            this.nutritionService = nutritionService;
+        }
+    }
+}
