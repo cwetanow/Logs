4282c7402eb7c5cfb107c0b8ef2116a4342677ea
Ivan <madritistaaa@gmail.com>
2017-04-30 14:20:19 +0300

    Add Input view and actions

--- src/Logs.Web/Controllers/NutritionController.cs
+++ src/Logs.Web/Controllers/NutritionController.cs
@@ -1,15 +1,21 @@
-﻿using System.Web.Mvc;
-using Logs.Services.Contracts;
+﻿using System;
+using System.Web.Mvc;
+using Logs.Web.Models.Nutrition;
 
 namespace Logs.Web.Controllers
 {
     public class NutritionController : Controller
     {
-        private readonly INutritionService nutritionService;
-
-        public NutritionController(INutritionService nutritionService)
+        public ActionResult Input()
         {
-            this.nutritionService = nutritionService;
+            var model = new InputViewModel { Date = DateTime.Now };
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Input(InputViewModel model)
+        {
+            return null;
         }
     }
 }
---
+++ src/Logs.Web/Models/Nutrition/InputViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Logs.Web.Models.Nutrition
+{
+    public class InputViewModel
+    {
+        [DisplayFormat(DataFormatString = "{0:dd/MMMM/yyyy}")]
+        public DateTime Date { get; set; }
+    }
+}
