b6934fb95e1beca0b77b77a46cbba8015ffa38e6
Ivan <madritistaaa@gmail.com>
2017-05-02 22:27:35 +0300

    Finish LoadEntry

--- src/Logs.Web/Controllers/NutritionController.cs
+++ src/Logs.Web/Controllers/NutritionController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.Mvc;
+using Logs.Authentication.Contracts;
 using Logs.Providers.Contracts;
 using Logs.Services.Contracts;
 using Logs.Web.Infrastructure.Factories;
@@ -7,19 +8,23 @@ using Logs.Web.Models.Nutrition;
 
 namespace Logs.Web.Controllers
 {
+    [Authorize]
     public class NutritionController : Controller
     {
         private readonly IViewModelFactory viewModelFactory;
         private readonly IDateTimeProvider dateTimeProvider;
         private readonly INutritionService nutritionService;
+        private readonly IAuthenticationProvider authenticationProvider;
 
         public NutritionController(IViewModelFactory viewModelFactory,
             IDateTimeProvider dateTimeProvider,
-            INutritionService nutritionService)
+            INutritionService nutritionService,
+            IAuthenticationProvider authenticationProvider)
         {
             this.viewModelFactory = viewModelFactory;
             this.dateTimeProvider = dateTimeProvider;
             this.nutritionService = nutritionService;
+            this.authenticationProvider = authenticationProvider;
         }
 
         public ActionResult Input()
@@ -34,6 +39,28 @@ namespace Logs.Web.Controllers
         [HttpPost]
         public ActionResult LoadEntry(InputViewModel model)
         {
+            if (this.ModelState.IsValid)
+            {
+
+                var userId = this.authenticationProvider.CurrentUserId;
+                var entry = this.nutritionService.GetEntryByDate(userId, model.Date);
+
+                var viewModel = new NutritionEntryViewModel();
+
+                if (entry != null)
+                {
+                    var measurementModel = this.viewModelFactory.CreateMeasurementViewModel(entry.Measurement);
+                    var nutritionModel = this.viewModelFactory.CreateNutritionViewModel(entry.Nutrition, entry.Notes);
+
+                    viewModel = this.viewModelFactory.CreateNutritionEntryViewModel(entry.NutritionEntryId,
+                        entry.Date,
+                        nutritionModel,
+                        measurementModel);
+                }
+                
+                return this.PartialView("NutritionEntryPartial", viewModel);
+            }
+
             return null;
         }
     }
