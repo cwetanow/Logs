a5f0e707f659031b58c234ac455588a1d737fe65
Ivan <madritistaaa@gmail.com>
2017-05-07 19:03:40 +0300

    Add rest of methods in NutritionService

--- src/Logs.Services/Contracts/INutritionService.cs
+++ src/Logs.Services/Contracts/INutritionService.cs
@@ -7,6 +7,15 @@ namespace Logs.Services.Contracts
     {
         NutritionEntry GetEntryByDate(string userId, DateTime date);
 
-        Nutrition EditNutrition(int id, string userId, DateTime date, int calories, int protein, int carbs, int fats, double water, int fiber, int sugar);
+        Nutrition UpdateNutrition(int id, string userId, DateTime date, int calories, int protein, int carbs, int fats, double water,
+            int fiber, int sugar, string notes);
+
+        Nutrition EditNutrition(int id, int calories, int protein, int carbs, int fats, double water, int fiber,
+            int sugar, string notes);
+
+        NutritionEntry CreateNutritionEntry(string userId, DateTime date);
+
+        Nutrition CreateNutrition(int calories, int protein, int carbs, int fats, double water, int fiber, int sugar,
+            string notes, NutritionEntry entry);
     }
 }
