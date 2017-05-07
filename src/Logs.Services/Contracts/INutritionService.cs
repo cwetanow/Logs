9c2f64ce4cdeab51ab609b91a153b2d31ca800d7
Ivan <madritistaaa@gmail.com>
2017-05-07 11:23:46 +0300

    EditNutrition method in service

--- src/Logs.Services/Contracts/INutritionService.cs
+++ src/Logs.Services/Contracts/INutritionService.cs
@@ -6,5 +6,7 @@ namespace Logs.Services.Contracts
     public interface INutritionService
     {
         NutritionEntry GetEntryByDate(string userId, DateTime date);
+
+        Nutrition EditNutrition(int id, string userId, DateTime date, int calories, int protein, int carbs, int fats, double water, int fiber, int sugar);
     }
 }
