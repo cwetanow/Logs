002ce6b8f1083fd91a06b72bdab883d75e212565
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-12 15:15:00 +0300

    Add factory method

--- src/Logs.Factories/IMeasurementFactory.cs
+++ src/Logs.Factories/IMeasurementFactory.cs
@@ -1,6 +1,23 @@
-﻿namespace Logs.Factories
+﻿using Logs.Models;
+
+namespace Logs.Factories
 {
     public interface IMeasurementFactory
     {
+        Measurement CreateMeasurement(int heigh,
+                  double weightKg,
+                  double bodyFatPercent,
+                  int chest,
+                  int shoulders,
+                  int forearm,
+                  int arm,
+                  int waist,
+                  int hips,
+                  int thighs,
+                  int calves,
+                  int neck,
+                  int wrist,
+                  int ankle,
+                  int nutritionEntryId);
     }
 }
