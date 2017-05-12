fa0ba97b0059523f88f5ade77ed42315bebc340c
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-12 14:34:54 +0300

    Constructor injection in service

--- src/Logs.Services/MeasurementService.cs
+++ src/Logs.Services/MeasurementService.cs
@@ -1,8 +1,47 @@
-﻿using Logs.Services.Contracts;
+﻿using Logs.Data.Contracts;
+using Logs.Factories;
+using Logs.Models;
+using Logs.Services.Contracts;
+using System;
 
 namespace Logs.Services
 {
     public class MeasurementService : IMeasurementService
     {
+        private readonly IRepository<Measurement> repository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly INutritionService nutritionService;
+        private readonly IMeasurementFactory factory;
+
+        public MeasurementService(IRepository<Measurement> repository,
+            IUnitOfWork unitOfWork,
+            INutritionService nutritionService,
+            IMeasurementFactory factory)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException(nameof(unitOfWork));
+            }
+
+            if (nutritionService == null)
+            {
+                throw new ArgumentNullException(nameof(nutritionService));
+            }
+
+            if (factory == null)
+            {
+                throw new ArgumentNullException(nameof(factory));
+            }
+
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+            this.nutritionService = nutritionService;
+            this.factory = factory;
+        }
     }
 }
