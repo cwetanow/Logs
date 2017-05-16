6ad373dfd95a897c7b248605b615f2c6202286ed
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-16 10:02:08 +0300

    Training log tests

---
+++ src/Logs.Models.Tests/TrainingLogTests/PropertiesTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Logs.Models.Tests.TrainingLogTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [TestCase(1)]
+        [TestCase(11)]
+        [TestCase(42)]
+        [TestCase(13)]
+        public void TestLastEntryId_ShouldInitializeCorrectly(int lastEntryId)
+        {
+            // Arrange
+            var log = new TrainingLog();
+
+            // Act
+            log.LastEntryId = lastEntryId;
+
+            // Assert
+            Assert.AreEqual(lastEntryId, log.LastEntryId);
+        }
+
+        [TestCase("d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public void TestUserId_ShouldInitializeCorrectly(string userId)
+        {
+            // Arrange
+            var log = new TrainingLog();
+
+            // Act
+            log.UserId = userId;
+
+            // Assert
+            Assert.AreEqual(userId, log.UserId);
+        }
+
+     [Test]
+        public void TestUsers_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var log = new TrainingLog();
+            var users = new List<User>();
+
+            // Act
+            log.Users = users;
+
+            // Assert
+            CollectionAssert.AreEqual(users, log.Users);
+        }
+    }
+}
