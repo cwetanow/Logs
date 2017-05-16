8ad4eda13ce6f630ebea3540b93024dc8ee4ba89
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-16 10:26:01 +0300

    Measurement tests

---
+++ src/Logs.Models.Tests/MeasurementTests/PropertiesTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Logs.Models.Tests.MeasurementTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [TestCase(1)]
+        [TestCase(999)]
+        [TestCase(326)]
+        [TestCase(1257)]
+        [TestCase(73)]
+        [TestCase(14)]
+        public void TestMeasurementsId_ShouldInitializeCorrectly(int measurementId)
+        {
+            // Arrange
+            var measurement = new Measurement();
+
+            // Act
+            measurement.MeasurementsId = measurementId;
+
+            // Assert
+            Assert.AreEqual(measurementId, measurement.MeasurementsId);
+        }
+
+        [Test]
+        public void TestUser_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var measurement = new Measurement();
+            var user = new User();
+
+            // Act
+            measurement.User = user;
+
+            // Assert
+            Assert.AreSame(user, measurement.User);
+        }
+    }
+}
