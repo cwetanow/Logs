9c1b2c8cd8ac046aa615d2b7cfe8fbfb37bf3488
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-16 12:14:39 +0300

    Constructor tests

---
+++ src/Logs.Web.Tests/Controllers/MeasurementControllerTests/ConstructorTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Logs.Authentication.Contracts;
+using Logs.Providers.Contracts;
+using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Logs.Web.Infrastructure.Factories;
+using Moq;
+using NUnit.Framework;
+
+namespace Logs.Web.Tests.Controllers.MeasurementControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassEverything_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedFactory = new Mock<IViewModelFactory>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedMeasurementService = new Mock<IMeasurementService>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            // Act
+            var controller = new MeasurementController(mockedAuthenticationProvider.Object,
+            mockedMeasurementService.Object, mockedFactory.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void TestConstructor_PassFactoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedMeasurementService = new Mock<IMeasurementService>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new MeasurementController(mockedAuthenticationProvider.Object,
+                    mockedMeasurementService.Object,
+                    null));
+        }
+
+        [Test]
+        public void TestConstructor_PassMeasurementServiceNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedFactory = new Mock<IViewModelFactory>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new MeasurementController(mockedAuthenticationProvider.Object,
+                  null,
+                  mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassAuthenticationProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedFactory = new Mock<IViewModelFactory>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedMeasurementService = new Mock<IMeasurementService>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new MeasurementController(null,
+                    mockedMeasurementService.Object,
+                    mockedFactory.Object));
+        }
+    }
+}
