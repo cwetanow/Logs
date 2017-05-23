7cd15579a276231063f85ca21410c4078de47bde
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-23 15:06:17 +0300

    Rest of nutrition controller tests

---
+++ src/Logs.Web.Tests/Controllers/NutritionControllerTests/IndexTests.cs
@@ -0,0 +1,33 @@
+﻿using Logs.Authentication.Contracts;
+using Logs.Providers.Contracts;
+using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Logs.Web.Infrastructure.Factories;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace Logs.Web.Tests.Controllers.NutritionControllerTests
+{
+    [TestFixture]
+    public class IndexTests
+    {
+        [Test]
+        public void TestIndex_ShouldRenderCorrectView()
+        {
+            // Arrange
+            var mockedFactory = new Mock<IViewModelFactory>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedNutritionService = new Mock<INutritionService>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            var controller = new NutritionController(mockedFactory.Object, mockedDateTimeProvider.Object,
+                mockedNutritionService.Object, mockedAuthenticationProvider.Object);
+
+            // Act, Assert
+            controller
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
