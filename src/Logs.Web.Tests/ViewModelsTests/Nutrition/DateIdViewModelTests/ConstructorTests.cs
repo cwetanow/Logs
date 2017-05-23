2c046adf4a5bac4d9b938e7f2c69ad9dde0534a4
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-23 15:11:34 +0300

    Vm constructor tests

---
+++ src/Logs.Web.Tests/ViewModelsTests/Nutrition/DateIdViewModelTests/ConstructorTests.cs
@@ -0,0 +1,45 @@
+﻿using Logs.Web.Models.Nutrition;
+using NUnit.Framework;
+
+namespace Logs.Web.Tests.ViewModelsTests.Nutrition.DateIdViewModelTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [TestCase(1, "formated date")]
+        [TestCase(171, "another formated date")]
+        [TestCase(3, "look, a formated date")]
+        public void TestConstructor_ShouldSetIdCorrectly(int id, string formatedDate)
+        {
+            // Arrange, Act
+            var model = new DateIdViewModel(id, formatedDate);
+
+            // Assert
+            Assert.AreEqual(id, model.Id);
+        }
+
+        [TestCase(1, "formated date")]
+        [TestCase(171, "another formated date")]
+        [TestCase(3, "look, a formated date")]
+        public void TestConstructor_ShouldSetDateCorrectly(int id, string formatedDate)
+        {
+            // Arrange, Act
+            var model = new DateIdViewModel(id, formatedDate);
+
+            // Assert
+            Assert.AreEqual(formatedDate, model.FormattedDate);
+        }
+
+        [TestCase(1, "formated date")]
+        [TestCase(171, "another formated date")]
+        [TestCase(3, "look, a formated date")]
+        public void TestConstructor_ShouldInitializeCorrectly(int id, string formatedDate)
+        {
+            // Arrange, Act
+            var model = new DateIdViewModel(id, formatedDate);
+
+            // Assert
+            Assert.IsNotNull(model);
+        }
+    }
+}
