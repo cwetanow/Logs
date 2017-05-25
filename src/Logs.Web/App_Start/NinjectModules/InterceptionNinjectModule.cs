421dda26ffb24babc0b21945594259c3c5d97326
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-25 10:19:13 +0300

    Add new interceptions

--- src/Logs.Web/App_Start/NinjectModules/InterceptionNinjectModule.cs
+++ src/Logs.Web/App_Start/NinjectModules/InterceptionNinjectModule.cs
@@ -20,6 +20,8 @@ namespace Logs.Web.App_Start.NinjectModules
             var cachingInterceptor = this.Kernel.Get<CachingInterceptor>();
 
             Kernel.AddMethodInterceptor(typeof(LogsService).GetMethod("GetAllSortedByDate"), cachingInterceptor.Intercept);
+            Kernel.AddMethodInterceptor(typeof(NutritionService).GetMethod("GetUserNutritionsSortedByDate"), cachingInterceptor.Intercept);
+            Kernel.AddMethodInterceptor(typeof(MeasurementService).GetMethod("GetUserMeasurementsSortedByDate"), cachingInterceptor.Intercept);
         }
     }
 }
