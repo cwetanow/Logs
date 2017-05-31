d392126562f53da15f38f22d531f9ad61b772f7f
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-31 16:51:15 +0300

    Add new vm

---
+++ src/Logs.Web/Areas/Users/Models/UserIdViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Logs.Web.Areas.Users.Models
+{
+    public class UserIdViewModel
+    {
+        public UserIdViewModel(string userId)
+        {
+            this.UserId = userId;
+        }
+
+        public string UserId { get; set; }
+    }
+}
