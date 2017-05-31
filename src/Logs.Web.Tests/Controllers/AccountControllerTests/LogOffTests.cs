f31bea1b0a5095d3c9402fb3296e48944a13bcb6
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-31 16:12:06 +0300

    LogOff tests

---
+++ src/Logs.Web.Tests/Controllers/AccountControllerTests/LogOffTests.cs
@@ -0,0 +1,44 @@
+﻿using Logs.Authentication.Contracts;
+using Logs.Factories;
+using Logs.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace Logs.Web.Tests.Controllers.AccountControllerTests
+{
+    [TestFixture]
+    public class LogOffTests
+    {
+        [Test]
+        public void TestLogOff_ShouldCallProviderSignOut()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            // Act
+            controller.LogOff();
+
+            // Assert
+            mockedProvider.Verify(p => p.SignOut(), Times.Once);
+        }
+
+        [Test]
+        public void TestLogOff_ShouldReturnRedirectToAction()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            // Act, Assert
+            controller
+                .WithCallTo(c => c.LogOff())
+                .ShouldRedirectTo((HomeController homeController) => homeController.Index());
+        }
+    }
+}
