e42ad4c2d6837a69775a904447f02d19e707392c
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-31 16:51:05 +0300

    Change area route

--- src/Logs.Web/Areas/Users/UsersAreaRegistration.cs
+++ src/Logs.Web/Areas/Users/UsersAreaRegistration.cs
@@ -2,22 +2,22 @@
 
 namespace Logs.Web.Areas.Users
 {
-    public class UsersAreaRegistration : AreaRegistration 
+    public class UsersAreaRegistration : AreaRegistration
     {
-        public override string AreaName 
+        public override string AreaName
         {
-            get 
+            get
             {
                 return "Users";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
                 "Users_default",
-                "Users/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                "Users/{controller}/{action}/{username}",
+                new { action = "Index" }
             );
         }
     }
