7a876351cfe358e197377312eca3ebc390c00d23
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-05-31 16:51:44 +0300

    Logic for controller

--- src/Logs.Web/Areas/Users/Controllers/NutritionController.cs
+++ src/Logs.Web/Areas/Users/Controllers/NutritionController.cs
@@ -1,8 +1,48 @@
-﻿using System.Web.Mvc;
+﻿using Logs.Services.Contracts;
+using Logs.Web.Infrastructure.Factories;
+using System;
+using System.Web.Mvc;
 
 namespace Logs.Web.Areas.Users.Controllers
 {
     public class NutritionController : Controller
     {
+        private readonly IUserService userService;
+        private readonly IViewModelFactory viewModelFactory;
+
+        public NutritionController(IUserService userService, IViewModelFactory viewModelFactory)
+        {
+            if (userService == null)
+            {
+                throw new ArgumentNullException(nameof(userService));
+            }
+
+            if (viewModelFactory == null)
+            {
+                throw new ArgumentNullException(nameof(viewModelFactory));
+            }
+
+            this.userService = userService;
+            this.viewModelFactory = viewModelFactory;
+        }
+
+        public ActionResult Details(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return new HttpNotFoundResult();
+            }
+
+            var user = this.userService.GetUserByUsername(username);
+
+            if (user == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            var model = this.viewModelFactory.CreateUserIdViewModel(user.Id);
+
+            return this.View(model);
+        }
     }
 }
