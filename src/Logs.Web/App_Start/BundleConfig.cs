a0356ad4659b7933a97e95627965e0f50b7ef183
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-06-02 15:15:49 +0300

    Add bundle

--- src/Logs.Web/App_Start/BundleConfig.cs
+++ src/Logs.Web/App_Start/BundleConfig.cs
@@ -31,6 +31,10 @@ namespace Logs.Web
                        "~/Scripts/jquery.cloudinary.js",
                        "~/Scripts/image-upload.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/tinymce").Include(
+                       "~/Scripts/tinymce/tinymce.min.js",
+                       "~/Scripts/editor-config.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
