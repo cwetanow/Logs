ef706a468fe2e4e0ce7eb64feb27d7f1177bad2d
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-06-02 15:15:41 +0300

    Add js file with rich text editor

---
+++ src/Logs.Web/Scripts/editor-config.js
@@ -0,0 +1,22 @@
+﻿tinymce.init({
+    selector: 'textarea',
+    height: 500,
+    theme: 'modern',
+    plugins: [
+      'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+      'searchreplace wordcount visualblocks visualchars code fullscreen',
+      'insertdatetime media nonbreaking save table contextmenu directionality',
+      'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'
+    ],
+    toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    toolbar2: 'preview media | forecolor backcolor | fontsizeselect',
+    image_advtab: false,
+    content_css: [
+      '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+      '//www.tinymce.com/css/codepen.min.css'
+    ],
+    branding: false,
+    menubar: false,
+    resize: false,
+    statusbar: false
+});
