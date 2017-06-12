0e744f6fc484c8d79fd0faad0d28d9c40bdbb4a8
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-06-12 16:29:27 +0300

    Controller constructor and subscribe tests

---
+++ src/Logs.Web.Tests/Controllers/SubscriptionControllerTests/ConstructorTests.cs
@@ -0,0 +1,62 @@
+﻿using Logs.Authentication.Contracts;
+using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Logs.Web.Infrastructure.Factories;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Logs.Web.Tests.Controllers.SubscriptionControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassEverythingCorrectly_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedService = new Mock<ISubscriptionService>();
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            // Act
+            var controller = new SubscriptionController(mockedService.Object, mockedProvider.Object, mockedFactory.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void TestConstructor_PassServiceNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new SubscriptionController(null, mockedProvider.Object, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedService = new Mock<ISubscriptionService>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new SubscriptionController(mockedService.Object, null, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassViewModelFactoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedService = new Mock<ISubscriptionService>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new SubscriptionController(mockedService.Object, mockedProvider.Object, null));
+        }
+    }
+}
---
+++ src/Logs.Web.Tests/Controllers/SubscriptionControllerTests/SubscribeTests.cs
@@ -0,0 +1,100 @@
+﻿using Logs.Authentication.Contracts;
+using Logs.Services.Contracts;
+using Logs.Web.Controllers;
+using Logs.Web.Infrastructure.Factories;
+using Logs.Web.Models.Subscription;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace Logs.Web.Tests.Controllers.SubscriptionControllerTests
+{
+    [TestFixture]
+    public class SubscribeTests
+    {
+        [TestCase(1, "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase(423, "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public void TestSubscribe_ShouldCallAuthProviderCurrentUserId(int logId, string userId)
+        {
+            // Arrange
+            var mockedService = new Mock<ISubscriptionService>();
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var controller = new SubscriptionController(mockedService.Object, mockedProvider.Object, mockedFactory.Object);
+
+            // Act
+            controller.Subscribe(logId);
+
+            // Assert
+            mockedProvider.Verify(p => p.CurrentUserId, Times.Once);
+        }
+
+        [TestCase(1, "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase(423, "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public void TestSubscribe_ShouldCallSubscriptionServiceSubscribe(int logId, string userId)
+        {
+            // Arrange
+            var mockedService = new Mock<ISubscriptionService>();
+
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            mockedProvider.Setup(p => p.CurrentUserId).Returns(userId);
+
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var controller = new SubscriptionController(mockedService.Object, mockedProvider.Object, mockedFactory.Object);
+
+            // Act
+            controller.Subscribe(logId);
+
+            // Assert
+            mockedService.Verify(s => s.Subscribe(logId, userId), Times.Once);
+        }
+
+        [TestCase(1, "d547a40d-c45f-4c43-99de-0bfe9199ff95", true)]
+        [TestCase(423, "99ae8dd3-1067-4141-9675-62e94bb6caaa", false)]
+        public void TestSubscribe_ShouldCallFactoryCreateSubscribeViewModel(int logId, string userId, bool isSubscribed)
+        {
+            // Arrange
+            var mockedService = new Mock<ISubscriptionService>();
+            mockedService.Setup(s => s.Subscribe(It.IsAny<int>(), It.IsAny<string>()))
+                .Returns(isSubscribed);
+
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            mockedProvider.Setup(p => p.CurrentUserId).Returns(userId);
+
+            var mockedFactory = new Mock<IViewModelFactory>();
+
+            var controller = new SubscriptionController(mockedService.Object, mockedProvider.Object, mockedFactory.Object);
+
+            // Act
+            controller.Subscribe(logId);
+
+            // Assert
+            mockedFactory.Verify(f => f.CreateSubscribeViewModel(isSubscribed), Times.Once);
+        }
+
+        [TestCase(1, "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase(423, "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public void TestSubscribe_ShouldRenderCorrectPartialViewWithModel(int logId, string userId)
+        {
+            // Arrange
+            var mockedService = new Mock<ISubscriptionService>();
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            var model = new SubscribeViewModel();
+
+            var mockedFactory = new Mock<IViewModelFactory>();
+            mockedFactory.Setup(f => f.CreateSubscribeViewModel(It.IsAny<bool>()))
+                .Returns(model);
+
+            var controller = new SubscriptionController(mockedService.Object, mockedProvider.Object, mockedFactory.Object);
+
+            // Act
+            controller
+                .WithCallTo(c => c.Subscribe(logId))
+                .ShouldRenderPartialView("_SubscriptionPartial")
+                .WithModel(model);
+        }
+    }
+}
