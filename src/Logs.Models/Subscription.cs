0592585e35c0213c8685da61a140f05ee5a0a781
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-06-12 15:47:52 +0300

    Add subscription model

---
+++ src/Logs.Models/Subscription.cs
@@ -0,0 +1,15 @@
+﻿namespace Logs.Models
+{
+    public class Subscription
+    {
+        public int SubscriptionId { get; set; }
+
+        public string UserId { get; set; }
+
+        public int TrainingLogId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual TrainingLog TrainingLog { get; set; }
+    }
+}
