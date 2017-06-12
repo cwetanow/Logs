afb588df43743cf4705c259ad141cf1b3ca14360
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-06-12 16:06:15 +0300

    Add subscription service interface

---
+++ src/Logs.Services/Contracts/ISubscriptionService.cs
@@ -0,0 +1,9 @@
+﻿namespace Logs.Services.Contracts
+{
+    public interface ISubscriptionService
+    {
+        bool Subscribe(int logId, string userId);
+
+        bool Unsubscribe(int logId, string userId);
+    }
+}
