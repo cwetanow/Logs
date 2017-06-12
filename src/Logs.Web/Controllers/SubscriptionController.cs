a6cb9e34474463e665556ec87d3561148bd86893
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-06-12 16:06:24 +0300

    Create subscription controller

---
+++ src/Logs.Web/Controllers/SubscriptionController.cs
@@ -0,0 +1,29 @@
+﻿using Logs.Authentication.Contracts;
+using Logs.Services.Contracts;
+using System;
+using System.Web.Mvc;
+
+namespace Logs.Web.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        private readonly ISubscriptionService subscriptionService;
+        private readonly IAuthenticationProvider authenticationProvider;
+
+        public SubscriptionController(ISubscriptionService subscriptionService, IAuthenticationProvider authenticationProvider)
+        {
+            if (subscriptionService == null)
+            {
+                throw new ArgumentNullException(nameof(subscriptionService));
+            }
+
+            if (authenticationProvider == null)
+            {
+                throw new ArgumentNullException(nameof(authenticationProvider));
+            }
+
+            this.subscriptionService = subscriptionService;
+            this.authenticationProvider = authenticationProvider;
+        }
+    }
+}
