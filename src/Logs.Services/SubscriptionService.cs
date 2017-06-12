8117e15562a5efa1424de14c4e0eceb4ed6b615b
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-06-12 16:42:28 +0300

    Add subscription service

---
+++ src/Logs.Services/SubscriptionService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Logs.Services.Contracts;
+using Logs.Data.Contracts;
+using Logs.Models;
+using Logs.Factories;
+
+namespace Logs.Services
+{
+    public class SubscriptionService : ISubscriptionService
+    {
+        private readonly IRepository<Subscription> repository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly ISubscriptionFactory factory;
+        
+        public bool IsSubscribed(int logId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Subscribe(int logId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Unsubscribe(int logId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
